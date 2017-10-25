9f4810b97b4d589686d04765f7df1c0430053a02
代码狮 <1173197065@qq.com>
2017-10-25 15:27:57 +0800

    简单的登录功能
    简单的登录，没有短信验证

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+		/* 数据库设置 */
+		'DB_TYPE' => 'mysql', // 数据库类型
+		'DB_HOST' => '127.0.0.1', // 服务器地址
+		'DB_NAME' => 'db_exam', // 数据库名
+		'DB_USER' => 'root', // 用户名
+		'DB_PWD' => '', // 密码
+		'DB_PORT' => '3306', // 端口
+		'DB_PREFIX' => 'mia', // 数据库表前缀
+		                      
+		// 动态加载文件
+		'LOAD_EXT_FILE' => 'info'  // 多个文件用英文半角逗号分隔
+
 );
